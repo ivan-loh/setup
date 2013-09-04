93182b1afcf11258e7261821f7c2577750391c6c
Ivan <ivan@eriad.com>
2013-09-04 11:22:24 +0800

    change to get node from repo instead of nvm

--- setup.sh
+++ setup.sh
@@ -2,16 +2,15 @@
 # Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
 # for headless setup. 
 
-# Install nvm: node-version manager
-# https://github.com/creationix/nvm
+# Install essentials
 sudo apt-get install -y git
 sudo apt-get install -y curl
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
-# Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+# Install nodejs
+sudo apt-get install -y python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
@@ -35,6 +34,7 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
+
 git clone https://github.com/ivan-loh/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
