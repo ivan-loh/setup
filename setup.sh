c63d985b67097fdf67e1954dc635aa3ad11e97f1
Ivan <ivan@eriad.com>
2013-11-07 03:17:55 +0800

    Update setup.sh
    consolidated update and install

--- setup.sh
+++ setup.sh
@@ -3,28 +3,27 @@
 # for headless setup. 
 
 # Install essentials
-sudo apt-get install -y git
-sudo apt-get install -y curl
+sudo apt-get install -y git curl python-software-properties python g++ make
 
-# Install nodejs
-sudo apt-get install -y python-software-properties python g++ make
+# Add Required repository
+sudo add-apt-repository -y ppa:cassou/emacs
 sudo add-apt-repository ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install nodejs
+
+# Update repo information
+sudo apt-get -qq update
+
+# 1. Install nodejs
+#
+# 2. Install emacs24
+# https://launchpad.net/~cassou/+archive/emacs
+#
+# 3. Install rlwrap to provide libreadline features with node
+# See: http://nodejs.org/api/repl.html#repl_repl
+sudo apt-get install -y nodejs emacs24-nox emacs24-el emacs24-common-non-dfsg rlwrap
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
 sudo npm install -g jshint
-
-# Install rlwrap to provide libreadline features with node
-# See: http://nodejs.org/api/repl.html#repl_repl
-sudo apt-get install -y rlwrap
-
-# Install emacs24
-# https://launchpad.net/~cassou/+archive/emacs
-sudo add-apt-repository -y ppa:cassou/emacs
-sudo apt-get -qq update
-sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
 
 # git pull and install dotfiles as well
 cd $HOME
